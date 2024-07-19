f814bf2eccfdd0b359efa733c8c1de25966457d3
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2024-07-19 16:53:01 +0300

    ⚰️ deleted network configuration

--- src/agent.ts
+++ src/agent.ts
@@ -15,19 +15,12 @@ import {
 import { NetworkManager } from "forta-agent-tools";
 import ethers from "forta-agent";
 
-import { networkData, NetworkData } from "./network";
 import {
   methods,
   fortaRegistryAddress,
   nethermindDeployAddress,
 } from "./constants";
 
-const networkManager = new NetworkManager(networkData); //global var
-
-// export const ERC20_TRANSFER_EVENT =
-//   "event Transfer(address indexed from, address indexed to, uint256 value)";
-// export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-// export const TETHER_DECIMALS = 6;
 let findingsCount = 0;
 
 export const provideHandleTransaction = (
@@ -89,36 +82,10 @@ export const provideHandleTransaction = (
   };
 };
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
-
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// };
-
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
-
-// const healthCheck: HealthCheck = async () => {
-//   const errors: string[] = [];
-// detect some health check condition
-// errors.push("not healthy due to some condition")
-// return errors;
-// }
-
 export default {
   handleTransaction: provideHandleTransaction(
     methods,
     nethermindDeployAddress,
     fortaRegistryAddress
   ),
-  // healthCheck,
-  // handleBlock,
-  // handleAlert
 };
